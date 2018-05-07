006bbef2443b41e0dfca2b7af762ec4475cbc059
Jason Kriss <jasonkriss@gmail.com>
2018-05-07 13:13:55 -0700

    make preprocessor a class

--- surefire/data/__init__.py
+++ surefire/data/__init__.py
@@ -8,6 +8,71 @@ import pandas as pd
 from surefire.features import CategoricalFeature, BinaryFeature, NumericalFeature
 
 
+def _group(l, key):
+    groups = {}
+    for item in l:
+        k = key(item)
+        if k in groups:
+            groups[k].append(item)
+        else:
+            groups[k] = [item]
+    return groups
+
+
+class Preprocessor(object):
+    def __init__(self, features):
+        self._features = features
+        self._mapper = None
+        self._vocabs = None
+
+    def fit(self, X):
+        self._mapper = self._build_mapper(X)
+        self._mapper.fit(X)
+        self._vocabs = self._build_vocabs(X)
+        return self
+
+    def transform(self, X):
+        X = self._mapper.transform(X)
+        return self._apply_vocabs(X)
+
+    def _build_mapper(self, X):
+        binary_features = [f.name for f in self._features if isinstance(f, BinaryFeature)]
+        categorical_features = [f.name for f in self._features if isinstance(f, CategoricalFeature)]
+        numerical_features = [f.name for f in self._features if isinstance(f, NumericalFeature)]
+        ignored_features = list(set(X.columns) - set([f.name for f in self._features]))
+        binary = [([f], Imputer(strategy='most_frequent')) for f in binary_features]
+        categorical = [(f, [CategoricalImputer()]) for f in categorical_features]
+        numerical = [([f], [Imputer(strategy='mean'), StandardScaler()]) for f in numerical_features]
+        ignored = [(f, None) for f in ignored_features]
+        return DataFrameMapper(binary + categorical + numerical + ignored, df_out=True)
+
+    def _grouped_categorical_features(self):
+        categorical_features = [f for f in self._features if isinstance(f, CategoricalFeature)]
+        return _group(categorical_features, itemgetter(1))
+
+    def _build_vocabs(self, X):
+        vocabs = {}
+        for family, features in self._grouped_categorical_features().items():
+            vocabs[family.name] = self._build_vocab(X, family, features)
+        return vocabs
+
+    def _build_vocab(self, X, family, features):
+        s = pd.Series()
+        for feature in features:
+            s = s.append(X[feature.name])
+        values = list(s.value_counts().index)
+        return values if len(values) <= family.cardinality else ['<UNK>'] + values[:(family.cardinality - 1)]
+
+    def _apply_vocabs(self, X):
+        for family, features in self._grouped_categorical_features().items():
+             self._apply_vocab(X, self._vocabs[family.name], features)
+        return X
+
+    def _apply_vocab(self, X, vocab, features):
+        for feature in features:
+            X[feature.name] = X[feature.name].apply(lambda value: vocab.index(value) if value in vocab else 0)
+
+
 class DataFrameDataset(Dataset):
     def __init__(self, X, y, transform=None, target_transform=None):
         super().__init__()
@@ -27,62 +92,3 @@ class DataFrameDataset(Dataset):
         if self._target_transform is not None:
             target = self._target_transform(target)
         return row, target
-
-
-def _group(l, key):
-    groups = {}
-    for item in l:
-        k = key(item)
-        if k in groups:
-            groups[k].append(item)
-        else:
-            groups[k] = [item]
-    return groups
-
-
-def _build_mapper(binary_features, categorical_features, numerical_features, ignored_features):
-    binary = [([f], Imputer(strategy='most_frequent')) for f in binary_features]
-    categorical = [(f, [CategoricalImputer()]) for f in categorical_features]
-    numerical = [([f], [Imputer(strategy='mean'), StandardScaler()]) for f in numerical_features]
-    ignored = [(f, None) for f in ignored_features]
-    return DataFrameMapper(binary + categorical + numerical + ignored, df_out=True)
-
-
-def _transform(raw, features):
-    binary_features = [f.name for f in features if isinstance(f, BinaryFeature)]
-    categorical_features = [f.name for f in features if isinstance(f, CategoricalFeature)]
-    numerical_features = [f.name for f in features if isinstance(f, NumericalFeature)]
-    ignored_features = list(set(raw.columns) - set([f.name for f in features]))
-    mapper = _build_mapper(binary_features, categorical_features, numerical_features, ignored_features)
-    transformed = mapper.fit_transform(raw)
-    transformed[numerical_features] = transformed[numerical_features].astype('float32')
-    transformed[binary_features] = transformed[binary_features].astype('float32')
-    return transformed
-
-
-def _build_vocab(df, family, features):
-    s = pd.Series()
-    for feature in features:
-        s = s.append(df[feature.name])
-    values = list(s.value_counts().index)
-    return values if len(values) <= family.cardinality else ['<UNK>'] + values[:(family.cardinality - 1)]
-
-
-def _apply_vocab(df, vocab, features):
-    for feature in features:
-        df[feature.name] = df[feature.name].apply(lambda value: vocab.index(value) if value in vocab else 0)
-
-
-def _vocabularize(df, features):
-    vocabs = {}
-    categorical_features = [f for f in features if isinstance(f, CategoricalFeature)]
-    for family, features in _group(categorical_features, itemgetter(1)).items():
-        vocab = _build_vocab(df, family, features)
-        _apply_vocab(df, vocab, features)
-        vocabs[family.name] = vocab
-    return df, vocabs    
-
-
-def preprocess(raw, features):
-    transformed = _transform(raw, features)
-    return _vocabularize(transformed, features)
