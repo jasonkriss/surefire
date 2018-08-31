f35b91822a44c4d725a53c05bf9070cae55f7973
Jason Kriss <jasonkriss@gmail.com>
2018-08-31 16:28:19 -0700

    remove linear layer and dropout from KimEncoder

--- surefire/encoders/kim.py
+++ surefire/encoders/kim.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Embedding, ModuleList, Dropout, Linear, Conv1d
+from torch.nn import Embedding, ModuleList, Conv1d
 from torch.nn.functional import relu, max_pool1d
 
 from surefire.encoders import Encoder
@@ -10,19 +10,15 @@ def _max_over_time_pool(input):
 
 
 class KimEncoder(Encoder):
-    def __init__(self, num_embeddings, embedding_dim, out_features, num_features=100, kernel_sizes=[3, 4, 5], dropout=0.5):
+    def __init__(self, num_embeddings, embedding_dim, num_features=100, kernel_sizes=[3, 4, 5]):
         super().__init__()
         self._embedding = Embedding(num_embeddings, embedding_dim, padding_idx=0)
         self._convs = ModuleList([Conv1d(embedding_dim, num_features, kernel_size) for kernel_size in kernel_sizes])
-        self._dropout = Dropout(dropout)
-        self._linear = Linear(num_features * len(kernel_sizes), out_features)
     
     def forward(self, x):
         embedded = torch.transpose(self._embedding(x), 1, 2)
         features = torch.cat([_max_over_time_pool(conv(embedded)) for conv in self._convs], dim=1)
-        features = relu(features)
-        features = self._dropout(features)
-        return self._linear(features)
+        return relu(features)
 
     def num_features(self):
-        return self._linear.out_features
+        return sum((conv.out_channels for conv in self._convs))
