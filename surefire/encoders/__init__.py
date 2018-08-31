528d489701ef654f6998b12591b8dac14b6a2ada
Jason Kriss <jasonkriss@gmail.com>
2018-08-31 10:57:16 -0700

    tweak import ordering

--- surefire/encoders/__init__.py
+++ surefire/encoders/__init__.py
@@ -1,5 +1,5 @@
+from surefire.encoders.encoder import Encoder
 from surefire.encoders.batchnorm import BatchNormEncoder
 from surefire.encoders.embedding import EmbeddingEncoder
-from surefire.encoders.encoder import Encoder
 from surefire.encoders.identity import IdentityEncoder
 from surefire.encoders.kim import KimEncoder
